850c03679d99b6be4b289da8e43a069b7200ccc6
Ben <34463636+Benji123abc@users.noreply.github.com>
2019-02-16 16:49:41 -0500

    Initial Commit

---
+++ ModMaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ModMaker
+//
+//  Created by Ben on 2/16/19.
+//  Copyright © 2019 Stormbreaker Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
