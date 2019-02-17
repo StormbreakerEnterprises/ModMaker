30f3bc5a8e021197d2ac820b139ffead8bad699e
Ben <34463636+Benji123abc@users.noreply.github.com>
2019-02-16 20:36:15 -0500

    Add Basic UI

--- ModMaker/ViewController.swift
+++ ModMaker/ViewController.swift
@@ -10,18 +10,40 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var fileURL: URL! = nil
+    var fileName : String! = nil
+    var stringToWrite : String! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        /*fileName = "testFile"
+        let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
+        
+        print("File Path: \(fileURL.path)")*/
     }
-
+    
+    func exportMod() {
+        /*stringToWrite = """
+        
+        """
+        do {
+            try stringToWrite.write(to: fileURL, atomically: true, encoding: .utf8)
+        } catch {
+            print(error)
+        }*/
+    }
+    
+    @IBAction func addObjectToMod(_ sender: Any) {
+    }
+    
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
-
